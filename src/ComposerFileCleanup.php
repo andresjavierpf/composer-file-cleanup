b16028edee337aed7993de7d5095f0545ec58912
Andrés J. Pavón Fernández <ajavier900710@gmail.com>
2023-08-17 06:45:33 -0400

    Add ComposerFileCleanup class

---
+++ src/ComposerFileCleanup.php
@@ -0,0 +1,48 @@
+<?php
+namespace AndrusDev\Composer;
+
+use Composer\Script\Event;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+
+class ComposerFileCleanup 
+{
+    public static function cleanup( Event $event ) {
+        $composer   = $event->getComposer();
+        $extra      = $composer->getPackage()->getExtra();
+        
+        $itemsToRemove = $extra['composer-file-cleanup'] ?? [];
+
+        if (!empty($itemsToRemove)) {
+            $vendorDir = $composer->getConfig()->get('vendor-dir');
+            $filesystem = new Filesystem();
+            $removedCount = self::removeItemsInPackages($vendorDir, $itemsToRemove, $filesystem);
+
+            if ($removedCount > 0) {
+                $event->getIO()->write(sprintf('Removed %s items', $removedCount));
+            }
+        }
+    }
+
+    private static function removeItemsInPackages( $vendorDir, array $itemsToRemove, Filesystem $filesystem ) {
+        $removedCount = 0;
+        $packagesDir = $vendorDir . '/';
+
+        foreach (glob($packagesDir . '*', GLOB_ONLYDIR) as $packageDir) {
+            foreach ($itemsToRemove as $item) {
+                $finder = (new Finder())
+                    ->files()
+                    ->name($item)
+                    ->in($packageDir);
+
+                foreach ($finder as $file) {
+                    $realpath = $file->getRealPath();
+                    $filesystem->remove($realpath);
+                    $removedCount++;
+                }
+            }
+        }
+
+        return $removedCount;
+    }
+}
